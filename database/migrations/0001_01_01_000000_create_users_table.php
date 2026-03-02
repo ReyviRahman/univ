df2fdcd87ffcb376bd105b1d0ae7df4b66c8478c
Reyvi Rahman <reyvirahman@gmail.com>
2026-03-02 11:52:15 +0700

    Add Tabel Migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nim');
+            $table->string('username')->unique(); // Bisa NIK atau NIM
             $table->string('email')->unique();
-            $table->string('name');
             $table->string('password');
-            $table->string('prodi');
-            $table->string('phone');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
+            $table->enum('role', ['admin', 'lecturer', 'student']); // Role akses
             $table->timestamps();
         });
 
---
+++ database/migrations/2026_03_02_044622_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users'); // Relasi ke Login
+            $table->foreignId('department_id')->constrained('departments'); // Relasi ke Prodi
+            $table->foreignId('advisor_id')->nullable()->constrained('lecturers'); // Dosen Wali (PA)
+            $table->string('nim', 20)->unique();
+            $table->string('name');
+            $table->string('pob')->nullable(); // Tempat Lahir
+            $table->date('dob')->nullable();   // Tanggal Lahir
+            $table->string('gender', 1);       // L/P
+            $table->string('phone', 20)->nullable();
+            $table->text('address')->nullable();
+            $table->year('entry_year');        // Angkatan (2023, 2024)
+            $table->enum('status', ['registered', 'non_active', 'active', 'leave', 'graduated', 'dropout'])->default('registered');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
